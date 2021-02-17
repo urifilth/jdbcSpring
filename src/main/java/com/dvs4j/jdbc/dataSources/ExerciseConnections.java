451b5b1cc1f952d957eb3da701a3991c85578f63
urifilth <uriel_filth11@hotmail.com>
2021-02-17 14:11:26 -0600

    conexiones hikari, pool, driver

--- src/main/java/com/dvs4j/jdbc/dataSources/ExerciseConnections.java
+++ src/main/java/com/dvs4j/jdbc/dataSources/ExerciseConnections.java
@@ -7,6 +7,10 @@ import java.sql.SQLException;
 import org.h2.jdbcx.JdbcConnectionPool;
 import org.h2.jdbcx.JdbcDataSource;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+
 public class ExerciseConnections {
 	
 	private static final int num_connections = 200;
@@ -25,19 +29,36 @@ public class ExerciseConnections {
 	 * Para 10 conexiones 174 ms
 	 * Para 30 conexiones 175 ms
 	 * Para 100 conexiones 180 ms
-	 *  
+	 * Para 200 conexiones 184 ms
+	 */
+	
+	/**
+	 * ---Con HikariCP---
+	 * 	Para 1 conexión 16 ms
+	 * 	Para 10 conexiones 16 ms
+	 *  Para 30 conexiones 17 ms
+	 *  Para 100 conexiones 17 ms
+	 *  Para 200 conexiones   
 	 */
 	
 	public static void main(String[] args) throws SQLException {
 		
-		JdbcConnectionPool connectionPool = JdbcConnectionPool.create("jdbc:h2:~/test","","");
+		HikariConfig config = new HikariConfig();
+		config.setJdbcUrl("jdbc:h2:~/test");
+		config.setUsername("");
+		config.setPassword("");
+		
+		HikariDataSource connectionPool = new HikariDataSource(config);
+		
+//ConectionPool		JdbcConnectionPool connectionPool = JdbcConnectionPool.create("jdbc:h2:~/test","","");
 		Long startTime = System.currentTimeMillis();
 		for(int i=0;i<num_connections;i++) {
-			//Connection connection = DriverManager.getConnection("jdbc:h2:~/test"); 
+//Conection Driver Manager			Connection connection = DriverManager.getConnection("jdbc:h2:~/test"); 
 			Connection connection = connectionPool.getConnection();
 			connection.close();
 		}
 		System.out.println("Total time; "+(System.currentTimeMillis()-startTime)+" milisegundos");
+		connectionPool.close();
 	}
 
 }
